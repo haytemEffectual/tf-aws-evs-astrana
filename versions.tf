076cd503efbfe561bdd859b512f9d928a1eeb809
Haytem <haytem.alsharif@effectual.com>
2026-01-22 17:36:05 +0000

    finalizing the repo structure

---
+++ versions.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+  backend "s3" {
+    # Backend configuration will be provided via terraform init flags or backend config file via GH Actions workflows
+  }
+}
